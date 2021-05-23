83c31825a59b80b51bfaf3ad891191cf3a2b9099
Ronnmarsh <84646808+ronnmarsh@users.noreply.github.com>
2021-05-23 11:21:46 -0300

    Add files via upload

---
+++ uri_1046/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace uri_1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //leia a hora inicial e final de um jogo
+            int hora_inicio, hora_fim, total;
+            string[] vetor = Console.ReadLine().Split(' ');
+
+            hora_inicio = int.Parse(vetor[0]);
+            hora_fim = int.Parse(vetor[1]);
+
+            //A seguir calcule a duração do jogo, sabendo que o mesmo pode
+            //começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas
+             
+            if (hora_inicio == hora_fim){
+                total = 24;
+            }
+            else if(hora_inicio > hora_fim){
+                total = (24 - hora_inicio) + (hora_fim);
+            }
+            else{
+                total = hora_fim - hora_inicio;
+            }
+            Console.WriteLine("O JOGO DUROU "+total+" HORA(S)");
+        }
+    }
+}
---
+++ uri_1048/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri_1048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario_atual = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double salario_reajustado, valor_adicional;
+            int percentual;
+
+            if(salario_atual <= 400.00){
+                percentual = 15;
+                valor_adicional = ((salario_atual/100) *percentual);
+                salario_reajustado = salario_atual + valor_adicional;
+            }
+            else if (salario_atual > 400.00 && salario_atual <= 800.00){
+                percentual = 12;
+                valor_adicional = ((salario_atual/100) *percentual);
+                salario_reajustado = salario_atual + valor_adicional;
+            }
+            else if(salario_atual > 800.00 && salario_atual <= 1200.00){
+                percentual = 10;
+                valor_adicional = ((salario_atual/100) *percentual);
+                salario_reajustado = salario_atual + valor_adicional;
+            }
+            else if(salario_atual > 1200.00 && salario_atual <= 2000.00){
+                percentual = 7;
+                valor_adicional = ((salario_atual/100) * percentual);
+                salario_reajustado = salario_atual + valor_adicional;
+            }
+            else{
+                percentual = 4;
+                valor_adicional = ((salario_atual/100) *percentual);
+                salario_reajustado = salario_atual + valor_adicional;
+            }
+            Console.WriteLine("Novo salario: "+salario_reajustado.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste ganho: "+valor_adicional.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Em percentual: "+percentual.ToString()+" %");
+
+
+        }
+    }
+}
