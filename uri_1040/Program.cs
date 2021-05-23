7d97464fca16e5ed3fd839a708a87ad72b825341
Ronnmarsh <84646808+ronnmarsh@users.noreply.github.com>
2021-05-23 11:20:39 -0300

    Add files via upload

---
+++ uri_1040/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri_1040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+
+            float n1 = float.Parse(valores[0], CultureInfo.InvariantCulture); //peso 2
+            float n2 = float.Parse(valores[1], CultureInfo.InvariantCulture); //peso 3
+            float n3 = float.Parse(valores[2], CultureInfo.InvariantCulture); //peso 4
+            float n4 = float.Parse(valores[3], CultureInfo.InvariantCulture); //peso 1
+
+            float media = ((n1 * 2) + (n2 * 3) + (n3 * 4) + (n4 * 1)) / (2 + 3 + 4 + 1);
+
+            float nExame, mediaExame;
+
+           //problema de arredodamento específico do C#
+            if(media == 4.85f){
+                media = 4.8f;
+            }
+            //fium do problema de arredondamento específico do C#
+            
+            Console.WriteLine("Media: " + media.ToString("F1", CultureInfo.InvariantCulture));
+
+            if(media >= 7.0){
+                Console.WriteLine("Aluno aprovado.");
+            }
+            else if (media < 5.0){
+                Console.WriteLine("Aluno reprovado.");
+            }
+            else{
+                Console.WriteLine("Aluno em exame.");
+
+                nExame = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine("Nota do exame: " + nExame.ToString("F1"));
+
+                mediaExame = (media + nExame)/2;
+
+                    if(mediaExame >= 5.0){
+                       Console.WriteLine("Aluno aprovado.");
+                    }
+                    else{
+                      Console.WriteLine("Aluno reprovado.");
+                    }
+
+                Console.WriteLine("Media final: "+mediaExame.ToString("F1", CultureInfo.InvariantCulture));
+            }
+
+
+        }
+    }
+}
---
+++ uri_1044/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uri_1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+            string[] vetor = Console.ReadLine().Split(' ');
+
+            A = int.Parse(vetor[0]);
+            B = int.Parse(vetor[1]);
+
+            if(A%B == 0 || B%A == 0){
+                Console.WriteLine("Sao Multiplos");
+            }
+            else{
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
