f44732ca86a43b78a374b2ed8e54b23ba1f1dc6d
Ronnmarsh <84646808+ronnmarsh@users.noreply.github.com>
2021-05-28 16:12:59 -0300

    Update Program.cs

--- uri_1047/Program.cs
+++ uri_1047/Program.cs
@@ -6,7 +6,33 @@ namespace uri_1047
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             //ler valores
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            int hora_inicio = int.Parse(vet[0]);
+            int minuto_inicio = int.Parse(vet[1]);
+            int hora_fim = int.Parse(vet[2]);
+            int minuto_fim = int.Parse(vet[3]);
+                    
+            int instanteInicial = hora_inicio * 60 + minuto_inicio;
+            int instanteFinal = hora_fim * 60 + minuto_fim;
+
+            int duracao; 
+
+            //calcular a duração
+          if (instanteInicial < instanteFinal) {
+                duracao = instanteFinal - instanteInicial;
+            }
+            else {
+                duracao = (24 * 60 - instanteInicial) + instanteFinal;
+            }
+
+            int horas = duracao /60;
+            int minutos = duracao % 60;
+            
+            //Saída
+            Console.WriteLine("O JOGO DUROU " + horas + " HORA(S) E " + minutos + " MINUTO(S)");
         }
     }
 }
